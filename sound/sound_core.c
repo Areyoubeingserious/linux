76f76dcd496ae53e6b270a098535f4dfbfede1a9
Areyoubeingserious <atxldn@gmail.com>
2026-07-03 11:16:18 +0100

    Areyoubeingserious Linux Kernel
    Areyoubeingserious Linux Kernel

--- sound/sound_core.c
+++ sound/sound_core.c
@@ -2,7 +2,7 @@
 /*
  *	Sound core.  This file is composed of two parts.  sound_class
  *	which is common to both OSS and ALSA and OSS sound core which
- *	is used OSS or emulation of it.
+ *	is used OSS or emulation of it. 
  */
 
 /*
